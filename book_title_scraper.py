f9f409af39dbb46a1ece6a9d9545433b8c9f99f1
sanhita <sanhita.joshi@gmail.com>
2023-01-01 17:35:46 -0600

    book scraper script; mostly works

---
+++ book_title_scraper.py
@@ -0,0 +1,230 @@
+from bs4 import BeautifulSoup
+import datetime
+import http
+import logging
+import numpy as np
+import pandas as pd
+import random
+import re
+import selenium
+from selenium import webdriver
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+
+class BookScraper:
+
+    def __init__(self):
+
+        self.books = pd.DataFrame()
+        self.book_mapping = pd.DataFrame()
+        self.book_info = {}
+
+        self.logger = logging.getLogger(__name__)
+
+        self.scraped_urls = set()
+        self.urls = []
+
+        extension = "_1_jan_00"
+        self.book_file = f"files/book_data{extension}.csv"
+        self.book_mapping_file = f"files/book_mapping{extension}.csv"
+
+        self.limit = 100_000
+
+    def make_soup(self, url: str = None) -> BeautifulSoup:
+        self.book_info["url"] = url
+
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+        driver.get(url)
+        source = driver.page_source
+        driver.close()
+        try:
+            soup = BeautifulSoup(source, features="html.parser")
+        except (http.client.IncompleteRead, selenium.common.exceptions.WebDriverException):
+            return
+        return soup
+
+    def extract_info_from_soup(self, soup: BeautifulSoup):
+
+        # book title
+        main_title = [item.strip() for item in soup.find(
+            'span', {'id': 'productTitle'}
+        )][0]
+        self.book_info["title"] = main_title
+
+        # book author
+        author = [item for item in soup.find_all(
+            'a',
+            {'class': "a-link-normal contributorNameID"}
+        )]
+        try:
+            self.book_info["author"] = author[0].text
+        except IndexError:
+            self.book_info["author"] = ""
+
+        num_reviews = [item for item in soup.find_all('a', {"id": "acrCustomerReviewLink"})]
+        try:
+            self.book_info["num_reviews"] = float(num_reviews[0].text.strip().split()[0].replace(',', ''))
+        except IndexError:
+            self.book_info["num_reviews"] = np.nan
+
+        try:
+            rating = ([item for item in soup.find_all(
+                'span',
+                {"class": "reviewCountTextLinkedHistogram noUnderline"}
+            )])[0].text.strip().replace(' out of 5 stars', '')
+        except IndexError:
+            rating = np.nan
+        self.book_info["rating"] = float(rating)
+
+        auth_about_class = "a-cardui-content a-cardui-uninitialized"
+        try:
+            about_auth = [item for item in soup.find_all("div", {"class": auth_about_class})][0].text
+        except IndexError:
+            about_auth = ""
+        self.book_info["about_author"] = about_auth
+
+        try:
+            book_description = [item for item in soup.find_all(
+                "div",
+                {"data-a-expander-name": "book_description_expander"}
+            )][0].text
+        except IndexError:
+            book_description = ""
+        self.book_info["book_description"] = book_description
+
+        # rankings in various categories. These can be used perhaps in clustering.
+        rankings = [item for item in soup.find_all(
+            "ul",
+            {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
+        )][1]
+        rankings = [item.strip() for item in rankings.text.strip().replace(
+            'Best Sellers Rank:', '').strip().split('#') if item.strip()]
+
+        remove_str = '(See Top 100 in Books)'
+
+        ranking_dict = {}
+        for item in rankings:
+            item = item.replace(remove_str, '').strip()
+            val, key = item[0], item[2:]
+            val = val.replace(",", "").replace("#", "")
+            ranking_dict[key.strip()] = int(val.strip())
+        self.book_info["rankings"] = ranking_dict
+
+        # metadata like publisher, book length, weight, publication date etc.
+        metadata = [item for item in soup.find_all(
+            "ul",
+            {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
+         )][0].text.replace('\n', ' ').replace('\u200f', ' ').replace('\u200e', '')
+
+        metadata = (re.split(': |\n|   |!|\+', metadata))
+        metadata = [item.strip() for item in metadata if item.strip()]
+        metadata_dict = {}
+
+        i = 0
+        if len(metadata) >= 2:
+            while i < len(metadata) - 1:
+                metadata_dict[metadata[i]] = metadata[i + 1]
+                i += 2
+
+        for k, v in metadata_dict.items():
+            if 'edition' in v:
+                dttime_string = v.split('edition')[-1].strip()
+                self.book_info["publication_date"] = self.string_time_to_datetime(dttime_string)
+            self.book_info[k] = v
+
+        books = [item for item in soup.find_all("div", {"class": "p13n-sc-uncoverable-faceout"})]
+
+        urls = []
+        for book in books:
+            url = book.a["href"]
+            idx = url.find("ref=")
+            urls.append(url[:idx])
+
+        titles = [book.img["alt"] for book in books]
+        if len(titles) < len(urls):
+            titles += [''] * [len(urls) - len(titles)]
+        try:
+            authors = [book.find(
+                "div",
+                {"class": "_cDEzb_p13n-sc-css-line-clamp-1_2o7X6"}
+            ).text for book in books]
+        except AttributeError:
+            try:
+                authors = [item.text for item in soup.find_all(
+                    "div",
+                    {"class": "_cDEzb_p13n-sc-css-line-clamp-1_2o7X6"}
+                )]
+            except AttributeError:
+                authors = []
+        if len(authors) < len(urls):
+            authors += [''] * [len(urls) - len(authors)]
+
+        mapped_books = []
+        for title, author, url in zip(titles, authors, urls):
+            # make sure the title or author match with the url.
+            if title.split()[0].lower() in url.lower() or author.split()[0].lower() in url.lower():
+                mapped_books.append([title, author, url])
+                self.urls.append(url)
+
+        mapped_books = pd.DataFrame(mapped_books, columns=["title", "author", "url"])
+        if mapped_books.any().any():
+            mapped_books.loc[:, "main_title"] = main_title
+
+        return mapped_books, pd.DataFrame.from_dict(self.book_info, orient="index").T
+
+    def string_time_to_datetime(self, date_string: str) -> datetime:
+
+        date_string = date_string.replace('(', '').replace(')', '').replace(',', '')
+        fmt = '%B %d %Y'
+        return datetime.datetime.strptime(date_string, fmt)
+
+    def scrape_for_books(self):
+        counter = 0
+        amazon_url = "https://www.amazon.com"
+        url = "/Walk-Blue-Fields-Claire-Keegan/dp/0802170498/"
+        link = f"{amazon_url}"
+
+        try:
+            self.books = pd.read_csv(self.book_file)
+            self.scraped_urls = set(self.books["url"].unique())
+        except FileNotFoundError:
+            self.books = pd.DataFrame()
+
+        try:
+            self.book_mapping = pd.read_csv(self.book_mapping_file)
+        except FileNotFoundError:
+            self.book_mapping = pd.DataFrame()
+
+        while counter < self.limit:
+
+            for url in self.urls[counter:]:
+                if url not in self.scraped_urls:
+                    break
+            link = amazon_url + url
+            soup = self.make_soup(link)
+            if soup:
+                mapped_books, df_book = self.extract_info_from_soup(soup)
+
+                self.scraped_urls.add(url)
+                self.books = pd.concat([self.books, df_book])
+                self.book_mapping = pd.concat([self.book_mapping, mapped_books])
+
+            counter += 1
+            time.sleep(random.randint(0, 5))
+
+            if counter % 5 == 0:
+                self.books.to_csv(self.book_file, index=False)
+                self.book_mapping.to_csv(self.book_mapping_file, index=False)
+                self.logger.info(f"URL settled on = {url}")
+
+        self.books.to_csv(self.book_file, index=False)
+        self.book_mapping.to_csv(self.book_mapping_file, index=False)
+
+
+if __name__ == '__main__':
+    bs = BookScraper()
+    bs.scrape_for_books()
