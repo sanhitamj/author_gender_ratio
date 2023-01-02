92fca39cf994c7f69dad9e5fd9ee85ee60bb91a8
sanhita <sanhita.joshi@gmail.com>
2023-01-02 17:32:44 -0600

    gitignore and scraper

--- book_title_scraper.py
+++ book_title_scraper.py
@@ -17,31 +17,46 @@ class BookScraper:
 
     def __init__(self):
 
-        self.books = pd.DataFrame()
-        self.book_mapping = pd.DataFrame()
-        self.book_info = {}
-
-        self.logger = logging.getLogger(__name__)
-
-        self.scraped_urls = set()
-        self.urls = []
-
-        extension = "_1_jan_00"
+        extension = "_2_jan_00"
         self.book_file = f"files/book_data{extension}.csv"
         self.book_mapping_file = f"files/book_mapping{extension}.csv"
+        self.logger = logging.getLogger(__name__)
+        self.amazon_url = "https://www.amazon.com"
 
-        self.limit = 100_000
+        try:
+            self.books = pd.read_csv(self.book_file)
+            self.books = self.books[~self.books["url"].duplicated()].copy()
+            self.scraped_urls = set(self.books["url"].unique())
+            self.logger.info(f"Found scraped data for {len(self.books)} books.")
+        except FileNotFoundError:
+            self.books = pd.DataFrame()
+            self.scraped_urls = set()
+
+        try:
+            self.book_mapping = pd.read_csv(self.book_mapping_file)
+            self.book_mapping = self.book_mapping[
+                (~self.book_mapping[["title", "main_title"]].duplicated())].copy()
+            self.urls = self.book_mapping["url"].to_list()
+            disjoint_set = set(self.urls).difference(self.scraped_urls)
+            self.logger.info(f"Number of URLs found and not scraped is {len(disjoint_set)}.")
+        except FileNotFoundError:
+            self.book_mapping = pd.DataFrame()
+            self.urls = []
+
+        self.book_info = {}
+        self.limit = 10_000
 
     def make_soup(self, url: str = None) -> BeautifulSoup:
+        link = self.amazon_url + url
         self.book_info["url"] = url
 
         options = webdriver.FirefoxOptions()
         options.add_argument("--headless")
         driver = webdriver.Firefox(options=options)
-        driver.get(url)
-        source = driver.page_source
-        driver.close()
         try:
+            driver.get(link)
+            source = driver.page_source
+            driver.close()
             soup = BeautifulSoup(source, features="html.parser")
         except (http.client.IncompleteRead, selenium.common.exceptions.WebDriverException):
             return
@@ -97,44 +112,49 @@ class BookScraper:
         self.book_info["book_description"] = book_description
 
         # rankings in various categories. These can be used perhaps in clustering.
-        rankings = [item for item in soup.find_all(
-            "ul",
-            {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
-        )][1]
-        rankings = [item.strip() for item in rankings.text.strip().replace(
-            'Best Sellers Rank:', '').strip().split('#') if item.strip()]
+        try:
+            rankings = [item for item in soup.find_all(
+                "ul",
+                {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
+            )][1]
+            rankings = [item.strip() for item in rankings.text.strip().replace(
+                'Best Sellers Rank:', '').strip().split('#') if item.strip()]
+            remove_str = '(See Top 100 in Books)'
 
-        remove_str = '(See Top 100 in Books)'
-
-        ranking_dict = {}
-        for item in rankings:
-            item = item.replace(remove_str, '').strip()
-            val, key = item[0], item[2:]
-            val = val.replace(",", "").replace("#", "")
-            ranking_dict[key.strip()] = int(val.strip())
-        self.book_info["rankings"] = ranking_dict
+            ranking_dict = {}
+            for item in rankings:
+                item = item.replace(remove_str, '').strip()
+                val, key = item[0], item[2:]
+                val = val.replace(",", "").replace("#", "")
+                ranking_dict[key.strip()] = int(val.strip())
+            self.book_info["rankings"] = ranking_dict
+        except IndexError:
+            self.book_info["rankings"] = {}
 
         # metadata like publisher, book length, weight, publication date etc.
-        metadata = [item for item in soup.find_all(
-            "ul",
-            {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
-         )][0].text.replace('\n', ' ').replace('\u200f', ' ').replace('\u200e', '')
+        try:
+            metadata = [item for item in soup.find_all(
+                "ul",
+                {"class": "a-unordered-list a-nostyle a-vertical a-spacing-none detail-bullet-list"}
+             )][0].text.replace('\n', ' ').replace('\u200f', ' ').replace('\u200e', '')
 
-        metadata = (re.split(': |\n|   |!|\+', metadata))
-        metadata = [item.strip() for item in metadata if item.strip()]
-        metadata_dict = {}
+            metadata = (re.split(': |\n|   |!|\+', metadata))
+            metadata = [item.strip() for item in metadata if item.strip()]
+            metadata_dict = {}
 
-        i = 0
-        if len(metadata) >= 2:
-            while i < len(metadata) - 1:
-                metadata_dict[metadata[i]] = metadata[i + 1]
-                i += 2
+            i = 0
+            if len(metadata) >= 2:
+                while i < len(metadata) - 1:
+                    metadata_dict[metadata[i]] = metadata[i + 1]
+                    i += 2
 
-        for k, v in metadata_dict.items():
-            if 'edition' in v:
-                dttime_string = v.split('edition')[-1].strip()
-                self.book_info["publication_date"] = self.string_time_to_datetime(dttime_string)
-            self.book_info[k] = v
+            for k, v in metadata_dict.items():
+                if 'edition' in v:
+                    dttime_string = v.split('edition')[-1].strip()
+                    self.book_info["publication_date"] = self.string_time_to_datetime(dttime_string)
+                self.book_info[k] = v
+        except IndexError:
+            self.book_info["publication_date"] = datetime.date.fromisoformat('1400-01-01')
 
         books = [item for item in soup.find_all("div", {"class": "p13n-sc-uncoverable-faceout"})]
 
@@ -146,7 +166,7 @@ class BookScraper:
 
         titles = [book.img["alt"] for book in books]
         if len(titles) < len(urls):
-            titles += [''] * [len(urls) - len(titles)]
+            titles += [''] * int(np.ceil([len(urls) - len(titles)]))
         try:
             authors = [book.find(
                 "div",
@@ -161,14 +181,12 @@ class BookScraper:
             except AttributeError:
                 authors = []
         if len(authors) < len(urls):
-            authors += [''] * [len(urls) - len(authors)]
+            authors += [''] * int(np.ceil([len(urls) - len(authors)]))
 
         mapped_books = []
         for title, author, url in zip(titles, authors, urls):
-            # make sure the title or author match with the url.
-            if title.split()[0].lower() in url.lower() or author.split()[0].lower() in url.lower():
-                mapped_books.append([title, author, url])
-                self.urls.append(url)
+            mapped_books.append([title, author, url])
+            self.urls.append(url)
 
         mapped_books = pd.DataFrame(mapped_books, columns=["title", "author", "url"])
         if mapped_books.any().any():
@@ -180,32 +198,27 @@ class BookScraper:
 
         date_string = date_string.replace('(', '').replace(')', '').replace(',', '')
         fmt = '%B %d %Y'
-        return datetime.datetime.strptime(date_string, fmt)
+        try:
+            date = datetime.datetime.strptime(date_string, fmt)
+        except ValueError:
+            date = datetime.date.fromisoformat('1400-01-01')
+        return date
 
     def scrape_for_books(self):
-        counter = 0
-        amazon_url = "https://www.amazon.com"
-        url = "/Walk-Blue-Fields-Claire-Keegan/dp/0802170498/"
-        link = f"{amazon_url}"
 
-        try:
-            self.books = pd.read_csv(self.book_file)
-            self.scraped_urls = set(self.books["url"].unique())
-        except FileNotFoundError:
-            self.books = pd.DataFrame()
+        counter = len(self.scraped_urls)
+        self.amazon_url = "https://www.amazon.com"
 
-        try:
-            self.book_mapping = pd.read_csv(self.book_mapping_file)
-        except FileNotFoundError:
-            self.book_mapping = pd.DataFrame()
+        # start with this URL if nothing scraped yet.
+        url = "/Future-Superhuman-transhuman-make-break/dp/1742236758"
 
         while counter < self.limit:
 
-            for url in self.urls[counter:]:
-                if url not in self.scraped_urls:
-                    break
-            link = amazon_url + url
-            soup = self.make_soup(link)
+            if len(self.urls) > 0:
+                for url in self.urls:
+                    if url not in self.scraped_urls:
+                        break
+            soup = self.make_soup(url)
             if soup:
                 mapped_books, df_book = self.extract_info_from_soup(soup)
 
@@ -213,13 +226,13 @@ class BookScraper:
                 self.books = pd.concat([self.books, df_book])
                 self.book_mapping = pd.concat([self.book_mapping, mapped_books])
 
-            counter += 1
-            time.sleep(random.randint(0, 5))
+                counter += 1
+                time.sleep(random.randint(0, 5))
 
-            if counter % 5 == 0:
-                self.books.to_csv(self.book_file, index=False)
-                self.book_mapping.to_csv(self.book_mapping_file, index=False)
-                self.logger.info(f"URL settled on = {url}")
+                if counter % 5 == 0:
+                    self.books.to_csv(self.book_file, index=False)
+                    self.book_mapping.to_csv(self.book_mapping_file, index=False)
+                    self.logger.info(f"Count #{counter} = {url}")
 
         self.books.to_csv(self.book_file, index=False)
         self.book_mapping.to_csv(self.book_mapping_file, index=False)
